7dc01ca027ee051007264254b8cf63b661fd9037
Lean <aleanraphael@gmail.com>
2018-06-08 08:39:46 +0800

    Add and Update
    Updated Find the smallest integer in the array.cs
    Added Return Negative.cs

---
+++ C#/8 Kyu/Return Negative.cs
@@ -0,0 +1,24 @@
+/*
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Example:
+
+Kata.MakeNegative(1); // return -1
+Kata.MakeNegative(-5); // return -5
+Kata.MakeNegative(0); // return 0
+
+Notes:
+
+    The number can be negative already, in which case no change is required.
+    Zero (0) can't be negative, see examples above.
+*/
+using System;
+
+public static class Kata
+{
+  public static int MakeNegative(int number)
+  {
+    if(number <= 0) return number;
+    return -number;
+  }
+}
