7e930797cac158eacc430f6a961b00e2bee6ae51
ClassicLean <aleanraphael@gmail.com>
2018-06-18 08:18:15 +0800

    Added multiplying two numbers.swift and Updated Switch it Up.swift

---
+++ Swift/8 Kyu/Function 3 - multiplying two numbers.swift
@@ -0,0 +1,6 @@
+/*
+This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+*/
+func multiply(_ a: Double, _ b: Double) -> Double {
+  return a * b    
+}
