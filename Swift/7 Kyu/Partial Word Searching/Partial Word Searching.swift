e530806c6e7d74897bb6763b3d836446ab64422d
ClassicLean <aleanraphael@gmail.com>
2018-06-19 22:32:19 +0800

    Added Partial Word Searching.swift

---
+++ Swift/7 Kyu/Partial Word Searching/Partial Word Searching.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func wordSearch(_ str:String, _ arr:[String]) -> [String] {
+  var result = [String]()
+  for word in arr {
+    if word.lowercased().range(of: str) != nil {
+      result.append(word)
+    }
+  }
+  if arr.count == 0 || result.count == 0 {
+    result.append("Empty")
+  }
+  return result
+}
