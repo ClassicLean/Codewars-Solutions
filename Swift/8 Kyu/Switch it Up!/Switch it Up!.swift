5e76ff9de031f0cfd869a47b104bf00ae6bc71ce
ClassicLean <aleanraphael@gmail.com>
2018-06-23 13:35:59 +0800

    Added Switch it Up folder

--- Swift/8 Kyu/Switch it Up!/Switch it Up!.swift
+++ Swift/8 Kyu/Switch it Up!/Switch it Up!.swift
@@ -1,12 +1,3 @@
-/*
-When provided with a number between 0-9, return it in words.
-
-Input :: 1
-
-Output :: "One".
-
-Try using "Switch" statements.
-*/
 func switchItUp(_ number: Int) -> String {
   return ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][number];
 }
