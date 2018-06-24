9b1e722f66e002303844a8ca24ab6c2ce651a635
ClassicLean <aleanraphael@gmail.com>
2018-06-24 09:36:38 +0800

    Updated ToLeetSpeak.swift

--- Swift/7 Kyu/ToLeetSpeak/ToLeetSpeak.swift
+++ Swift/7 Kyu/ToLeetSpeak/ToLeetSpeak.swift
@@ -1,62 +1,32 @@
 func toLeetSpeak(_ s : String) -> String {
-  var result: String = ""
-  for char in s.characters.indices{
-    switch s[char]{
-      case "A" :
-        result += "@"
-      case "B" :
-        result += "8"
-      case "C" :
-        result += "("
-      case "D" :
-        result += "D"
-      case "E" :
-        result += "3"
-      case "F" :
-        result += "F"
-      case "G" :
-        result += "6"
-      case "H" :
-        result += "#"
-      case "I" :
-        result += "!"
-      case "J" :
-        result += "J"
-      case "K" :
-        result += "K"
-      case "L" :
-        result += "1"
-      case "M" :
-        result += "M"
-      case "N" :
-        result += "N"
-      case "O" :
-        result += "0"
-      case "P" :
-        result += "P"
-      case "Q" :
-        result += "Q"
-      case "R" :
-        result += "R"
-      case "S" :
-        result += "$"
-      case "T" :
-        result += "7"
-      case "U" :
-        result += "U"
-      case "V" :
-        result += "V"
-      case "W" :
-        result += "W"
-      case "X" :
-        result += "X"
-      case "Y" :
-        result += "Y"
-      case "Z" :
-        result += "2"
-      default:
-        result += " "
-    }
-  }
-  return result
+  let leetAlphabet: [Character : Character] = [
+    " ": " ",
+    "A" : "@",
+    "B" : "8",
+    "C" : "(",
+    "D" : "D",
+    "E" : "3",
+    "F" : "F",
+    "G" : "6",
+    "H" : "#",
+    "I" : "!",
+    "J" : "J",
+    "K" : "K",
+    "L" : "1",
+    "M" : "M",
+    "N" : "N",
+    "O" : "0",
+    "P" : "P",
+    "Q" : "Q",
+    "R" : "R",
+    "S" : "$",
+    "T" : "7",
+    "U" : "U",
+    "V" : "V",
+    "W" : "W",
+    "X" : "X",
+    "Y" : "Y",
+    "Z" : "2",
+  ]
+  return String(s.characters.flatMap{leetAlphabet[$0]})
 }
