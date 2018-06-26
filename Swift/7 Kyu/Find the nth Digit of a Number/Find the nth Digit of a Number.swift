65a863c5dd50955e0fb506382364e89fc58e98f1
ClassicLean <aleanraphael@gmail.com>
2018-06-26 09:01:42 +0800

    Updated Find the nth Digit of a Number.swift

--- Swift/7 Kyu/Find the nth Digit of a Number/Find the nth Digit of a Number.swift
+++ Swift/7 Kyu/Find the nth Digit of a Number/Find the nth Digit of a Number.swift
@@ -1,11 +1,7 @@
 func findDigit(_ num:Int, _ nth: Int) -> Int {
-  var numArray = String(abs(num)).flatMap{Int(String($0))}
-  if nth > numArray.count{
-    return 0
-  }
-  if nth < 1{
-    return -1
-  }
-  numArray.reverse();
-  return numArray[nth - 1]
+  let positiveNum = abs(num);
+  guard nth > 0 else { return -1 }
+  guard positiveNum > 0 else { return 0 }
+  guard nth > 1 else { return positiveNum % 10 }
+  return findDigit(positiveNum / 10, nth - 1)
 }
