6b1725a3b85dab3c2ffe60f015d4e5a61053c57c
ClassicLean <aleanraphael@gmail.com>
2018-07-01 20:59:02 +0800

    Updated Mumbling.swift

--- Swift/7 Kyu/Mumbling/Mumbling.swift
+++ Swift/7 Kyu/Mumbling/Mumbling.swift
@@ -1,11 +1,6 @@
 func accum(_ s: String) -> String {
-  var result: String = ""
-  let letters = Array(s.characters)
-  for counter in stride(from: 1, through: s.characters.count, by: 1){
-    result += String(letters[counter - 1]).uppercased()
-    result += String(repeating: String(letters[counter - 1]).lowercased(), count: counter - 1)
-    if counter == s.characters.count { break }
-    result += "-"
-  }
-  return result
+  return s.characters.enumerated().map{
+    (counter, letter) -> String in
+    return String(repeating: String(letter), count: counter + 1).capitalized
+  }.joined(separator: "-")
 }
