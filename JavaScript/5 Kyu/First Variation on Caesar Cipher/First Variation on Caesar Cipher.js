429d95ef2652dca428d95b89105c2de10b502c7c
ClassicLean <aleanraphael@gmail.com>
2018-07-01 17:44:16 +0800

    Updated First Variation on Caesar Cipher.js

--- JavaScript/5 Kyu/First Variation on Caesar Cipher/First Variation on Caesar Cipher.js
+++ JavaScript/5 Kyu/First Variation on Caesar Cipher/First Variation on Caesar Cipher.js
@@ -1,67 +1,35 @@
+let upAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let downAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+
 function movingShift(s, shift) {
-  let sArray = s.split('');
-  let letters = /^[A-Za-z]+$/;
-  for(let char in sArray){
-    let smallChar = 122 - sArray[char].charCodeAt(0);
-    let bigChar = 90 - sArray[char].charCodeAt(0);
-    while(shift > 26) shift -= 26;
-    if(!sArray[char].match(letters)){
-      shift++;
-      continue;
-    }
-    if(sArray[char].charCodeAt(0) + shift > 90 &&
-      sArray[char].charCodeAt(0) <= 90 &&
-      sArray[char].charCodeAt(0) > 64){
-      sArray[char] = String.fromCharCode(Math.abs(bigChar - shift) + 64);
-    }
-    else if(sArray[char].charCodeAt(0) + shift > 122 &&
-      sArray[char].charCodeAt(0) <= 122 &&
-      sArray[char].charCodeAt(0) > 96){
-      sArray[char] = String.fromCharCode(Math.abs(smallChar - shift) + 96);
-      }
-    else sArray[char] = String.fromCharCode(sArray[char].charCodeAt(0) + shift);
-    shift++;
-  }
-  return arraySplit(sArray.join(''));
+    return arraySplit(letterShift(s,shift,1));
 }
 
 function demovingShift(arr, shift) {
-  let sArray = arr.join('').split('');
-  let letters = /^[A-Za-z]+$/;
-  for(let char in sArray){
-    let smallChar = 96 - sArray[char].charCodeAt(0);
-    let bigChar = 64 - sArray[char].charCodeAt(0);
-    while(shift > 26) shift -= 26;
-    if(!sArray[char].match(letters)){
-      shift++;
-      continue;
-    }
-    if(sArray[char].charCodeAt(0) - shift < 65 &&
-      sArray[char].charCodeAt(0) <= 90 &&
-      sArray[char].charCodeAt(0) > 64){
-      sArray[char] = String.fromCharCode(Math.abs(90 - bigChar - shift));
-    }
-    else if(sArray[char].charCodeAt(0) - shift < 97 &&
-      sArray[char].charCodeAt(0) <= 122 &&
-      sArray[char].charCodeAt(0) > 96){
-      sArray[char] = String.fromCharCode(Math.abs(122 - smallChar - shift));
-      }
-    else sArray[char] = String.fromCharCode(sArray[char].charCodeAt(0) - shift);
-    shift++;
-  }
-  return sArray.join('');
+    return letterShift(arr.join(''), -shift, -1);
 }
 
-function arraySplit(word){
-  let result = [];
-  word = word.split('');
-  let wordLen = word.length;
-  let div = Math.ceil(word.length / 5);
-  while(wordLen > 0){
-    result.push(word.slice(0,div).join(''));
-    word.slice(0,div).forEach(() => word.shift());
-    wordLen -= div;
-  }
-  for(let counter = result.length; counter < 5; counter++) result.push("");
-  return result;
+function letterShift(s,shift,step){
+  return s.toString().split('').map(function(letter, counter){
+    let upIndex = upAlphabet.indexOf(letter);
+    let downIndex = downAlphabet.indexOf(letter);
+    let library, libraryIndex;
+    if(upIndex == -1 && downIndex == -1) return letter;
+    if(upIndex > -1){
+      library = upAlphabet;
+      libraryIndex = upIndex;
+    }
+    else{
+      library = downAlphabet;
+      libraryIndex = downIndex;
+    }
+    let location = (counter * step + libraryIndex + shift) % 26;
+    location = location >= 0 ? location : location + 26;
+    return library[location];
+  }).join('');
+}
+
+function arraySplit(s){
+  let size = Math.ceil(s.length / 5);
+  return [s.slice(0, size), s.slice(size, size * 2), s.slice(size * 2, size * 3), s.slice(size * 3, size * 4), s.slice(size * 4)];
 }
