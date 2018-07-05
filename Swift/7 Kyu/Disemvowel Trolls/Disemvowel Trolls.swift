31380ecccaf419c4b444b2aa921366e592bffda7
ClassicLean <aleanraphael@gmail.com>
2018-07-05 09:16:38 +0800

    Updated Disemvowel Trolls.swift

--- Swift/7 Kyu/Disemvowel Trolls/Disemvowel Trolls.swift
+++ Swift/7 Kyu/Disemvowel Trolls/Disemvowel Trolls.swift
@@ -1,11 +1,4 @@
 func disemvowel(_ s: String) -> String {
-  var result: String = ""
-  let letters = Array(s.characters)
-  for counter in stride(from: 1, through: s.characters.count, by: 1) {
-    switch String(letters[counter - 1]).lowercased(){
-      case "a", "e", "i", "o", "u" : continue
-      default : result += String(letters[counter - 1])
-    }
-  }
-  return result
+  let vowels: [Character] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+  return String(s.characters.filter{!vowels.contains($0)})
 }
