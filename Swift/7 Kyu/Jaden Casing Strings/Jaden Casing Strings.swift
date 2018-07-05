2123a20492e44c0246238e099518e71bedd6b805
ClassicLean <aleanraphael@gmail.com>
2018-07-05 09:01:50 +0800

    Updated Jaden Casing Strings.swift

--- Swift/7 Kyu/Jaden Casing Strings/Jaden Casing Strings.swift
+++ Swift/7 Kyu/Jaden Casing Strings/Jaden Casing Strings.swift
@@ -2,11 +2,6 @@ import Foundation
 
 extension String {
     func toJadenCase() -> String {
-      var result: String = ""
-      let stringArray = self.characters.split{$0 == " "}.map(String.init)
-      for counter in stride(from: 1, through: stringArray.count, by: 1){
-        result += stringArray[counter - 1].capitalized + " "
-      }
-      return result.trimmingCharacters(in: .whitespacesAndNewlines)
+      return capitalized
     }
 }
