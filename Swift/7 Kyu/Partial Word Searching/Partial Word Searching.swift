0d4787a8ab4c1b09d9f0cb807d85e235c9a51065
ClassicLean <aleanraphael@gmail.com>
2018-07-08 21:47:48 +0800

    Updated Partial Word Searching.swift

--- Swift/7 Kyu/Partial Word Searching/Partial Word Searching.swift
+++ Swift/7 Kyu/Partial Word Searching/Partial Word Searching.swift
@@ -1,15 +1,4 @@
-//TODO fix. Works on other online compilers but not on codewars
-import Foundation
-
 func wordSearch(_ str:String, _ arr:[String]) -> [String] {
-  var result = [String]()
-  for word in arr {
-    if word.lowercased().range(of: str) != nil {
-      result.append(word)
-    }
-  }
-  if arr.count == 0 || result.count == 0 {
-    result.append("Empty")
-  }
-  return result
+    let result = arr.filter({ $0.lowercased().contains(str.lowercased()) })
+    return result.isEmpty ? ["Empty"] : result
 }
