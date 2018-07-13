4d12c97d62d9648cd6275930e81c8466e675c2e5
ClassicLean <aleanraphael@gmail.com>
2018-07-13 09:52:51 +0800

    Updated Vowel Count.py

--- Python/7 Kyu/Vowel Count/Vowel Count.py
+++ Python/7 Kyu/Vowel Count/Vowel Count.py
@@ -1,8 +1,2 @@
 def getCount(inputStr):
-    num_vowels = 0
-
-    for letter in inputStr:
-        if letter.lower() in ('a', 'e', 'i', 'o', 'u'):
-            num_vowels += 1
-
-    return num_vowels
+    return sum(1 for letter in inputStr if letter.lower() in "aeiou")
