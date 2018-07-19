0bded29a20734862fe4dbb224056d70da9308af9
ClassicLean <aleanraphael@gmail.com>
2018-07-19 10:09:50 +0800

    Updated Exes and Ohs.py

--- Python/7 Kyu/Exes and Ohs/Exes and Ohs.py
+++ Python/7 Kyu/Exes and Ohs/Exes and Ohs.py
@@ -1,10 +1,2 @@
 def xo(s):
-    sArr = list(s)
-    x = 0
-    o = 0
-    for letter in sArr:
-        if letter.lower() == "x":
-            x += 1
-        elif letter.lower() == "o":
-            o += 1
-    return True if x == o or ( x == 0 and o == 0 ) else False
+    return s.lower().count('x') == s.lower().count('o')
