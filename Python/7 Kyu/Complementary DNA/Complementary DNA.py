a511e626eca799480f1f0bed1595ecc9a22bc4fa
ClassicLean <aleanraphael@gmail.com>
2018-07-19 19:05:00 +0800

    Updated Complementary DNA.py

--- Python/7 Kyu/Complementary DNA/Complementary DNA.py
+++ Python/7 Kyu/Complementary DNA/Complementary DNA.py
@@ -1,13 +1,2 @@
 def DNA_strand(dna):
-    dnaArr = list(dna)
-    result = ""
-    for letter in dnaArr:
-      if letter == "A":
-          result += "T"
-      elif letter == "T":
-          result += "A"
-      elif letter == "C":
-          result += "G"
-      elif letter == "G":
-          result += "C"
-    return result
+    return dna.translate(str.maketrans('ATCG', 'TAGC'))
