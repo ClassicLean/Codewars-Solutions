19ae742fe1c9af967083228839e3bf13726b5873
ClassicLean <aleanraphael@gmail.com>
2018-07-20 10:45:11 +0800

    Updated Correct the time string.swift

--- Swift/7 Kyu/Correct the time-string/Correct the time-string.swift
+++ Swift/7 Kyu/Correct the time-string/Correct the time-string.swift
@@ -1,38 +1,14 @@
 func correct(_ timeString: String?) -> String? {
-  let newTime = (timeString ?? "")
-  if newTime.isEmpty { return timeString }
-  let timeAlphabet = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", ":"]
-  let timeFilter = String(newTime.filter{timeAlphabet.contains(String($0))})
-  if(timeFilter.count < 8) { return nil }
-  var timeArr = newTime.split(separator: ":").map{Int($0)!}
+  guard let time = timeString, time.isEmpty == false
+    else { return timeString }
 
-  while timeArr[2] >= 60 {
-      timeArr[2] -= 60
-      timeArr[1] += 1
-  }
+  let numbers = time.components(separatedBy: ":").flatMap({Int($0)})
+  guard numbers.count == 3
+    else { return nil }
 
-  while timeArr[1] >= 60 {
-      timeArr[1] -= 60
-      timeArr[0] += 1
-  }
-
-  while timeArr[0] >= 24 && timeArr[2] > 0 && timeArr[1] > 0 {
-      timeArr[0] -= 24
-  }
-
-  var newtimeArr = timeArr.map{String($0)}
-
-  if Int(newtimeArr[0])! < 10 {
-    newtimeArr[0] = "0" + newtimeArr[0]
-  }
-
-  if Int(newtimeArr[1])! < 10 {
-    newtimeArr[1] = "0" + newtimeArr[1]
-  }
-
-  if Int(newtimeArr[2])! < 10 {
-    newtimeArr[2] = "0" + newtimeArr[2]
-  }
-
-  return newtimeArr.joined(separator: ":")
+  let timeStamp = numbers[2] + numbers[1] * 60 + numbers[0] * 3600
+  let hours = (timeStamp / 3600) % 24
+  let minutes = (timeStamp / 60) % 60
+  let seconds = timeStamp % 60
+  return "\(String(format: "%02d", hours)):\(String(format: "%02d", minutes)):\(String(format: "%02d", seconds))"
 }
