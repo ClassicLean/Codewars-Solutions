56d74f21822006723fe36e6c2efb168c3b330bd3
ClassicLean <aleanraphael@gmail.com>
2018-07-21 11:55:39 +0800

    Updated Numbers with this digit inside.swift

--- Swift/7 Kyu/Numbers with this digit inside/Numbers with this digit inside.swift
+++ Swift/7 Kyu/Numbers with this digit inside/Numbers with this digit inside.swift
@@ -1,14 +1,6 @@
 func numbersWithDigitInside(_ x: Int64, _ d: Int64) -> [Int64] {
-  let numArr = (1...x).map{String($0)}.filter{$0.contains(String(d))}
-  guard numArr.count > 0 else { return [0,0,0] }
-
-  let numSum = numArr.map{Int($0)!}.reduce(0, +)
-  let numProd = numArr.map{Int($0)!}.reduce(1, *)
-  var result: [Int64] = []
-
-  result.append(Int64(numArr.count))
-  result.append(Int64(numSum))
-  result.append(Int64(numProd))
-
-  return result
+  let numbers = (1...x).filter{ String($0).contains(String(d)) }
+  return [Int64(numbers.count),
+    numbers.reduce(Int64(0), +),
+    numbers.count > 0 ? numbers.reduce(Int64(1), *) : Int64(0)]
 }
