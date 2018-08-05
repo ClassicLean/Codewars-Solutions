d9f298257e8686c21c1ea0828715c41c969a0451
ClassicLean <aleanraphael@gmail.com>
2018-08-05 10:26:58 +0800

    Updated Count the Digit.swift

--- Swift/7 Kyu/Count the Digit/Count the Digit.swift
+++ Swift/7 Kyu/Count the Digit/Count the Digit.swift
@@ -1,3 +1,3 @@
 func nbDig(_ n: Int, _ d: Int) -> Int {
-  return (0...n).map{ String($0 * $0).filter{ $0 == Character(String(d)) } }.flatMap{ $0 }.count
+  return (0...n).flatMap{ String($0 * $0).filter{ $0 == Character(String(d)) } }.count
 }
