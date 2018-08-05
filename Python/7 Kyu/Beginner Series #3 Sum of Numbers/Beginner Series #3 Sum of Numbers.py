085f826f24da3615facecbc4bb5e707a3d6cf6e0
ClassicLean <aleanraphael@gmail.com>
2018-08-05 10:16:35 +0800

    Updated Beginner Series 3 Sum of Numbers.py

--- Python/7 Kyu/Beginner Series #3 Sum of Numbers/Beginner Series #3 Sum of Numbers.py
+++ Python/7 Kyu/Beginner Series #3 Sum of Numbers/Beginner Series #3 Sum of Numbers.py
@@ -1,7 +1,2 @@
 def get_sum(a,b):
-    minimum = min(a, b)
-    maximum = max(a, b)
-    result = minimum;
-    for counter in range(minimum, maximum):
-        result += counter + 1
-    return result
+    return sum(range(min(a, b), max(a, b) + 1))
