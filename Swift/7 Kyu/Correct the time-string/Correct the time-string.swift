1ff77828d8d4b8003351e92209b959953a027e5a
ClassicLean <aleanraphael@gmail.com>
2018-08-07 21:58:23 +0800

    Updated Correct the time-string.swift

--- Swift/7 Kyu/Correct the time-string/Correct the time-string.swift
+++ Swift/7 Kyu/Correct the time-string/Correct the time-string.swift
@@ -1,14 +1,12 @@
-func correct(_ timeString: String?) -> String? {
-  guard let time = timeString, time.isEmpty == false
-    else { return timeString }
-
-  let numbers = time.components(separatedBy: ":").flatMap({Int($0)})
-  guard numbers.count == 3
-    else { return nil }
-
-  let timeStamp = numbers[2] + numbers[1] * 60 + numbers[0] * 3600
-  let hours = (timeStamp / 3600) % 24
-  let minutes = (timeStamp / 60) % 60
-  let seconds = timeStamp % 60
-  return "\(String(format: "%02d", hours)):\(String(format: "%02d", minutes)):\(String(format: "%02d", seconds))"
+func fizzBuzzCuckooClock(_ time: String) -> String {
+    let components = time.components(separatedBy: ":")
+    let minute = Int(components[1])!
+    if [0, 30].contains(minute) {
+        let hour = Int(components[0])!
+        let count = minute == 30 ? 1 : (hour == 0 ? 12 : (hour > 12 ? hour - 12 : hour))
+        return (0..<count).map({ _ in "Cuckoo" }).joined(separator: " ")
+    } else {
+        let buffer = [(3, "Fizz"), (5, "Buzz")].flatMap({ minute % $0 == 0 ? $1 : nil })
+        return buffer.isEmpty ? "tick" : buffer.joined(separator: " ")
+    }
 }
