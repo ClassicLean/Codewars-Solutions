8d05be469c35bf8fcb54140d3846a5a6680c5f77
ClassicLean <aleanraphael@gmail.com>
2018-09-28 21:04:28 +0800

    Updated Fizz Buzz Cuckoo Clock.swift

--- Swift/7 Kyu/Fizz Buzz Cuckoo Clock/Fizz Buzz Cuckoo Clock.swift
+++ Swift/7 Kyu/Fizz Buzz Cuckoo Clock/Fizz Buzz Cuckoo Clock.swift
@@ -1,14 +1,12 @@
 func fizzBuzzCuckooClock(_ time: String) -> String {
-  let components = time.components(separatedBy: ":")
-  let minute = Int(components[1])!
-
+  let numbers = time.components(separatedBy: ":").flatMap{ Int($0) }
+  let minute = numbers[1]
   if [0, 30].contains(minute) {
-    let hour = Int(components[0])!
+    let hour = numbers[0]
     let count = minute == 30 ? 1 : (hour == 0 ? 12 : (hour > 12 ? hour - 12 : hour))
     return (0..<count).map{ _ in "Cuckoo" }.joined(separator: " ")
-  }
-  else {
-    let buffer = [(3, "Fizz"), (5, "Buzz")].flatMap{ minute % $0 == 0 ? $1 : nil }
-    return buffer.isEmpty ? "tick" : buffer.joined(separator: " ")
+  } else {
+      let buffer = [(3, "Fizz"), (5, "Buzz")].flatMap{ minute % $0 == 0 ? $1 : nil }
+      return buffer.isEmpty ? "tick" : buffer.joined(separator: " ")
   }
 }
